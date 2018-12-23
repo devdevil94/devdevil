c62ac4c64609ca3e7ac6b4e8023f86b0d4b1cbaa
Saud <saudd2gh@gmail.com>
2018-12-23 13:34:03 +0400

    basic functions added

---
+++ wp-content/themes/main-theme/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function theme_files(){
+    wp_enqueue_style('bootstrap-css','//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
+    wp_enqueue_script('bootstrap-js','//stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js', array( 'jquery' ), true);
+    wp_enqueue_style('main-style', get_template_directory_uri() . '/style.css', false, '1.0', 'all');
+}
+
+function theme_features(){
+
+}
+
+
+add_action('after_setup_theme', 'theme_features');
+add_action('wp_enqueue_scripts', 'theme_files'); 
+?>
