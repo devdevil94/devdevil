971ead1bcdb75f1f00326ba1465ebd4ae068be6a
Saud <saudd2gh@gmail.com>
2018-12-27 20:50:26 +0400

    Node initialized + loaders installed

--- wp-content/themes/main-theme/functions.php
+++ wp-content/themes/main-theme/functions.php
@@ -1,5 +1,4 @@
 <?php
-
 function load_bootstrap(){
     // wp_enqueue_style('bootstrap-css','//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
     // wp_enqueue_script('bootstrap-js','//stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js', array( 'jquery' ), true);
@@ -13,10 +12,11 @@ function load_fontawesome(){
 }
 
 function load_custom_files(){     
-    //wp_enqueue_style('style', get_stylesheet_directory_uri().'/style.css');
+    wp_enqueue_script('responsive-nav-menu', get_template_directory_uri().'/js/menu.js');
+    wp_enqueue_style('main-stylesheet', get_stylesheet_uri(), NULL, microtime());
 }
 
-add_action('wp_enqueue_scripts', 'load_bootstrap');
+//add_action('wp_enqueue_scripts', 'load_bootstrap');
 add_action('wp_enqueue_scripts', 'load_custom_files');
 add_action('wp_enqueue_scripts', 'load_fontawesome');
 ?>
--- wp-content/themes/main-theme/header.php
+++ wp-content/themes/main-theme/header.php
@@ -1,47 +1,34 @@
 <!DOCTYPE html>
 
 
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php wp_head(); ?>
-    <link href="<?php bloginfo('stylesheet_url') ?>" rel="stylesheet">
 </head>
 <body <?php body_class(); ?> >
 
 <!--Home page style-->
 <header>
-    <nav class="navbar navbar-expand-sm navbar-default fixed-top">
-        <a class="navbar-brand" href="#">
-            <img src="<?php echo get_theme_file_uri('/images/logo.png') ?>" alt="devDevil">
-        </a>
-        
-        <button class="navbar-toggler" data-toggle="collapse" data-target="#mainMenu">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div id="mainMenu" class="collapse navbar-collapse">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-                <!-- <li class="nav-item"><a class="nav-link" href="#">Projects</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Portfolio</a></li> -->
-                <li class="nav-item"><a class="nav-link" href="#">Blog</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">About</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
-            </ul>
-        </div> 
-    </nav>
-
-    <div id="banner">
-        <div class="banner-content">
-            <h1>Saud Al Alawi</h1>
-            <h3>Electronics hobbyist, and freelance web developer</h3>
-            <a href="#" class="btn btn-default btn-md">My Portfolio</a>
+    <nav>
+        <div class="logo">
+            <h4>devDevil</h4>
         </div>
-    </div>
+        <ul class="nav-links">
+            <li><a href="">Home</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Projects</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+        <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+    </nav>
 </header>
 
 
