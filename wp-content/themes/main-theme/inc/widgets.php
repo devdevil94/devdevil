1253a63e3eab81cbedcc9ba37f45f7e7378d7d18
Saud <saudd2gh@gmail.com>
2019-02-18 23:29:30 +0400

    back end display of the  popular posts widget completed

--- wp-content/themes/main-theme/inc/widgets.php
+++ wp-content/themes/main-theme/inc/widgets.php
@@ -14,14 +14,29 @@
         }
         //Handling the back-end display
         public function form($instance){
+            $title = (!empty($instance['title']) ? $instance['title'] : 'Popular Posts');
+            $total = (!empty($instance['total']) ? absint($instance['total']) : 4);
+            $thisTitleId = esc_attr($this->get_field_id('title'));
 
+            $output = '<p>';
+            $output .= '<label for="'.$thisTitleId.'">Title:</label>';
+            $output .= '<input type="text" class="widefat" id="'.$thisTitleId.'" name="'.$thisTitleId.'" value="'.esc_attr($title).'">';
+            $output .='</p>';
+
+            $thisTotalId = esc_attr($this->get_field_id('total'));
+
+            $output .= '<p>';
+            $output .= '<label for="'.$thisTotalId.'">Number of Posts:</label>';
+            $output .= '<input type="number" class="widefat" id="'.$thisTotalId.'" name="'.$thisTotalId.'" value="'.esc_attr($total).'">';
+            $output .='</p>';
+
+            echo $output;
         }
+
 
     }
 
-    add_action('widgets_init', function(){
-        register_widget('Devdevil_Popular_Posts');
-    });
+    add_action('widgets_init', function(){register_widget('Devdevil_Popular_Posts');});
 
     function store_post_views($postId){
         $metaKey = 'devdevil_post_views';
@@ -35,5 +50,6 @@
         // echo $views;
     }
     remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+
     
 ?>
