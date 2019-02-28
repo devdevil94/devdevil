e9e6a33cbd46f1fefc3395761ba3d5ca3679927a
Saud <saudd2gh@gmail.com>
2019-02-28 11:32:50 +0400

    social media accounts widget styling modified

--- wp-content/themes/main-theme/inc/templates/devdevil-social-media.php
+++ wp-content/themes/main-theme/inc/templates/devdevil-social-media.php
@@ -8,8 +8,9 @@ get_template_directory_uri().'/css/social-media-accounts.css'
 <div id="social-icons-container">
     <h3 class="social-heading">Follow Me</h3>
     <div class="social-icons">
-        <div class="fab fa-facebook"></div>
-        <div class="fab fa-twitter"></div>
-        <div class="fab fa-github"></div>
+        <a href="#"><div class="fab fa-facebook fa-2x"></div></a>
+        <a href="#"><div class="fab fa-twitter fa-2x"></div></a>
+        <a href="#"><div class="fab fa-google fa-2x"></div></a>
+        <a href="#"><div class="fab fa-github fa-2x"></div></a>
     </div>
 </div>
