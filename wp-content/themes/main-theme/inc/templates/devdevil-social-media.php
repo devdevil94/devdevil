af1dde6660e7c5209e0c9797a1f9ca27ca5d7b12
Saud <saudd2gh@gmail.com>
2019-03-02 10:45:48 +0400

    header styling modified

--- wp-content/themes/main-theme/inc/templates/devdevil-social-media.php
+++ wp-content/themes/main-theme/inc/templates/devdevil-social-media.php
@@ -1,14 +1,16 @@
 <?php
 wp_enqueue_style('social-accounts-style', 
 get_template_directory_uri().'/css/social-media-accounts.css'
-,array(), version_id(), 'all'); 
+,array(), microtime(), 'all'); 
 ?>
 
 
 <div id="social-icons-container">
     <h3 class="social-heading">Follow Me</h3>
     <div class="social-icons">
-        <a href="#"><div class="fab fa-facebook fa-2x"></div></a>
+        <a href="mailto:devdevilstudio@gmail.com">
+            <div class="fa fa-envelope fa-2x"></div>
+        </a>
         <a href="#"><div class="fab fa-twitter fa-2x"></div></a>
         <a href="#"><div class="fab fa-google fa-2x"></div></a>
         <a href="#"><div class="fab fa-github fa-2x"></div></a>
