3d0c4624725d80dc68a6d6bb6830533eb2b2d542
Saud <saudd2gh@gmail.com>
2019-03-03 11:16:27 +0400

    single post info section added

--- wp-content/themes/main-theme/single.php
+++ wp-content/themes/main-theme/single.php
@@ -9,7 +9,11 @@
     <div id="single-post-body">
         <div class="single-post-container">
             <h2 class="single-post-title"><?php the_title(); ?></h2>
-            <p class="single-post-content"><?php the_content(); ?></p>
+            <p class="single-post-info">
+                Posted on <?php the_date(); ?>
+                by <a href="#"><?php the_author(); ?></a>
+            </p>
+            <div class="single-post-content"><?php the_content(); ?></div>
         </div>     
 <?php       
     }
