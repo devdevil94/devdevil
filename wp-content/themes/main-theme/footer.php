047d20839e7cab6bde6aca5935329d8f417ce251
Saud <saudd2gh@gmail.com>
2019-03-04 09:01:45 +0400

    footer contacts added and footer styling modified

--- wp-content/themes/main-theme/footer.php
+++ wp-content/themes/main-theme/footer.php
@@ -2,6 +2,13 @@
 
 <footer>
     <div class="footer-container">
+        <div class="footer-contacts">
+            <a href="#"><div class="fab fa-github fa-2x"></div></a>
+            <a href="#"><div class="fab fa-linkedin fa-2x"></div></a>
+            <a href="mailto:devdevilstudio@gmail.com">
+                <div class="fa fa-envelope fa-2x"></div>
+            </a>
+        </div>
         <div id="privacy">
             <a href="<?php echo site_url('/privacy-policy'); ?>">Privacy Policy</a>
         </div>
--- wp-content/themes/main-theme/inc/templates/devdevil-social-media.php
+++ wp-content/themes/main-theme/inc/templates/devdevil-social-media.php
@@ -8,11 +8,8 @@ get_template_directory_uri().'/css/social-media-accounts.css'
 <div id="social-icons-container">
     <h3 class="social-heading">Follow Me</h3>
     <div class="social-icons">
-        <a href="mailto:devdevilstudio@gmail.com">
-            <div class="fa fa-envelope fa-2x"></div>
-        </a>
         <a href="#"><div class="fab fa-twitter fa-2x"></div></a>
         <a href="#"><div class="fab fa-google fa-2x"></div></a>
-        <a href="#"><div class="fab fa-github fa-2x"></div></a>
+        <a href="#"><div class="fab fa-facebook fa-2x"></div></a>
     </div>
 </div>
