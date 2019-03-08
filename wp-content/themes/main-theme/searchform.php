19caad00dfb1f3532c3feddbe71d1b5847b63c3a
Saud <saudd2gh@gmail.com>
2019-03-08 20:44:13 +0400

    search form style complete

--- wp-content/themes/main-theme/searchform.php
+++ wp-content/themes/main-theme/searchform.php
@@ -1,9 +1,10 @@
 
 <div class="search-form-container">
     <h3 class="search-form-heading">Search</h3>
-    <form role="search" method="get" action="<?php echo home_url('/'); ?>">
+    <form role="search" method="get" class="devdevil-search-form"
+    action="<?php echo home_url('/'); ?>">
         <input type="search" class="search-form-text" placeholder="Search"
         value="<?php echo get_search_query(); ?>" name="s" title="Search" />
-        <input type="submit" class="search-btn" value="Search" />
+        <input type="submit" class="search-btn" value="&#128269" />
     </form>
 </div>
