3bb21ed05dd0fbfb6e0ae7072375f64035d61065
Saud <saudd2gh@gmail.com>
2019-03-11 00:11:29 +0400

    single post pagination issue fixed

--- wp-content/themes/main-theme/content-single-pagination.php
+++ wp-content/themes/main-theme/content-single-pagination.php
@@ -1,17 +1,19 @@
 <?php               
     if(get_previous_post()){
+        $trimedTitle = wp_trim_words(get_the_title(get_previous_post()), 5);
 ?>
         <div class="post-prev">
-            <?php previous_post_link('&laquo; %link'); ?>
+            <?php previous_post_link('&laquo; %link', $trimedTitle, false); ?>
         </div>
 <?php
     } 
 ?>
 <?php               
     if(get_next_post()){
+        $trimedTitle = wp_trim_words(get_the_title(get_next_post()), 5);
 ?>
         <div class="post-next">
-            <?php next_post_link('%link &raquo;'); ?>
+            <?php next_post_link('%link &raquo;', $trimedTitle, false); ?>
         </div>
 <?php
     } 
