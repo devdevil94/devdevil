1d370afec9dab3f2034d4ee70d17af323bf5cd31
Saud <saudd2gh@gmail.com>
2019-03-11 03:16:21 +0400

    customized social media accounts links

--- wp-content/themes/main-theme/inc/templates/devdevil-social-media.php
+++ wp-content/themes/main-theme/inc/templates/devdevil-social-media.php
@@ -1,15 +1,25 @@
 <?php
 wp_enqueue_style('social-accounts-style', 
 get_template_directory_uri().'/css/social-media-accounts.css'
-,array(), microtime(), 'all'); 
+,array(), microtime(), 'all');
+
+$twitterLink = 'https://www.twitter.com/'.esc_attr(get_option('twitter'));
+$googleLink = 'https://www.google.com/'.esc_attr(get_option('google'));
+$facebookLink = 'https://www.facebook.com/'.esc_attr(get_option('facebook'));
 ?>
 
 
 <div id="social-icons-container">
     <h3 class="social-heading">Follow Me</h3>
     <div class="social-icons">
-        <a href="#"><div class="fab fa-twitter fa-2x"></div></a>
-        <a href="#"><div class="fab fa-google fa-2x"></div></a>
-        <a href="#"><div class="fab fa-facebook fa-2x"></div></a>
+        <a href="<?php echo $twitterLink; ?>">
+            <i class="fab fa-twitter fa-2x"></i>
+        </a>
+        <a href="<?php echo $googleLink; ?>">
+            <i class="fab fa-google fa-2x"></i>
+        </a>
+        <a href=" <?php echo $facebookLink; ?>">
+            <i class="fab fa-facebook fa-2x"></i>
+        </a>
     </div>
 </div>
